b036deef72104fa53f9b2b6643f74d52784eab3c
quincep <christen@christenward.com>
2017-09-09 13:15:43 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# christenward.com
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,422 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Christen Ward</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Theme CSS -->
+    <link href="css/freelancer.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet"
+          type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="page-top" class="index">
+<div id="skipnav"><a href="#maincontent">Skip to main content</a></div>
+
+<!-- Navigation -->
+<nav id="mainNav" class="navbar navbar-default navbar-fixed-top navbar-custom">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header page-scroll">
+            <button type="button" class="navbar-toggle" data-toggle="collapse"
+                    data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+            </button>
+            <a class="navbar-brand" href="#page-top">Christen Ward</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li class="hidden">
+                    <a href="#page-top"></a>
+                </li>
+                <li class="page-scroll">
+                    <a href="#about">About</a>
+                </li>
+                <li class="page-scroll">
+                    <a href="#experience">Experience</a>
+                </li>
+                <li class="page-scroll">
+                    <a href="#portfolio">Projects</a>
+                </li>
+                <li class="page-scroll">
+                    <a href="#contact">Contact</a>
+                </li>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container-fluid -->
+</nav>
+
+<!-- Header -->
+<header>
+    <div class="container" id="maincontent" tabindex="-1">
+        <div class="row">
+            <div class="col-lg-12">
+                <img class="img-responsive" id="profile" src="img/profile.png" style="max-width: 256px; max-height: 256px;" alt="">
+                <div class="intro-text">
+                    <h1 class="name">Christen Ward</h1>
+                    <hr class="star-light">
+                    <span class="skills">Mobile Developer - Full Stack Engineer</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+
+<!-- About Section -->
+<section id="about">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h2>About</h2>
+                <hr class="star-primary">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2">
+                <p>My name is Christen Ward. I'm a software engineer with industry experience in mobile and full
+                    stack web development, including Swift/iOS, Java/Android, and PHP/jQuery/HTML/CSS.</p>
+                <p>I received a BS in Mathematics and Statistics and a minor in Computer Science at University of
+                    Missouri – Kansas City in 2015. In addition to my studies, I also worked as an Undergraduate
+                    Learning Assistant in the mathematics department, tutoring college algebra.</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="experience">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h2>Experience</h2>
+                <hr class="star-primary">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-7 col-lg-offset-1">
+                <p><strong>Accelerated Development Group - Mobile Applications Engineer</strong>
+                </p><p>
+                    Developed a floor plan drawing application for iOS using Swift/SpriteKit that allows users to design plans by
+                    drawing and snapping objects to a grid, and syncs the drawings across multiple devices to be accessed anywhere.
+                </p><p>
+                    Developed a web-based CRM application in a 2-person team, using PHP, HTML/CSS, Javascript, and
+                    responsive front-end framework Foundation.
+                </p><p>
+                    Developed a RESTful API supporting JSON, which was utilized by mobile applications.
+                </p><p>
+                    <strong>ActiveLogic Labs - Software Engineer</strong>
+                </p><p>
+                    Developed a mobile application for two platforms (iOS/Android) using Java and Swift that remotely controls
+                    an IP camera.
+                </p><p>
+                    Developed an internal business improvement application for iOS using Swift that allows employees to
+                    recommend and vote on workplace suggestions.
+                </p><p>
+                    Implemented and designed RESTful APIs to support mobile applications.
+                    Used Laravel to develop web applications.
+                </p><p>
+                    <strong>UMKC Department of Mathematics - Undergraduate Learning Assistant</strong>
+
+                </p><p>
+                    Courses: Math 110 - College Algebra, Math 100 - Intermediate Algebra
+                </p><p>
+                    Provided on-demand tutoring for mathematics classes, helping teaching assistants answer student questions
+                    and assisting students with the required material for the course.
+                </p>
+            </div>
+            <div class="col-lg-3">
+                <p>August 2016 – Present</p> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br>
+                <p>April 2016 – July 2016</p> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br>
+                <p style="margin-top: 9px">January 2014 – May 2015</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Portfolio Grid Section -->
+<section id="portfolio">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h2>Projects</h2>
+                <hr class="star-primary">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-4 portfolio-item">
+                <a href="#portfolioModal1" class="portfolio-link" data-toggle="modal">
+                    <div class="caption">
+                        <div class="caption-content">
+                            <i class="fa fa-search-plus fa-3x"></i>
+                        </div>
+                    </div>
+                    <img src="img/portfolio/echo.png" class="img-responsive" alt="Echo">
+                </a>
+            </div>
+            <div class="col-sm-4 portfolio-item">
+                <a href="#portfolioModal2" class="portfolio-link" data-toggle="modal">
+                    <div class="caption">
+                        <div class="caption-content">
+                            <i class="fa fa-search-plus fa-3x"></i>
+                        </div>
+                    </div>
+                    <img src="img/portfolio/dc.png" class="img-responsive" alt="Dungeon Crawl">
+                </a>
+            </div>
+            <div class="col-sm-4 portfolio-item">
+                <a href="#portfolioModal3" class="portfolio-link" data-toggle="modal">
+                    <div class="caption">
+                        <div class="caption-content">
+                            <i class="fa fa-search-plus fa-3x"></i>
+                        </div>
+                    </div>
+                    <img src="img/portfolio/space_small.png" class="img-responsive" alt="Space Roguelike">
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Contact Section -->
+<section id="contact">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h2>Contact Me</h2>
+                <hr class="star-primary">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-7 col-lg-offset-2">
+                <form name="sentMessage" id="contactForm">
+                    <div class="row control-group">
+                        <div class="form-group col-xs-12 floating-label-form-group controls">
+                            <label for="name">Name</label>
+                            <input name="name" type="text" class="form-control" placeholder="Name" id="name" required
+                                   data-validation-required-message="Please enter your name.">
+                            <p class="help-block text-danger"></p>
+                        </div>
+                    </div>
+                    <div class="row control-group">
+                        <div class="form-group col-xs-12 floating-label-form-group controls">
+                            <label for="email">Email Address</label>
+                            <input type="email" name="email" class="form-control" placeholder="Email Address" id="email" required
+                                   data-validation-required-message="Please enter your email address.">
+                            <p class="help-block text-danger"></p>
+                        </div>
+                    </div>
+                    <div class="row control-group">
+                        <div class="form-group col-xs-12 floating-label-form-group controls">
+                            <label for="phone">Phone Number</label>
+                            <input name="phone" type="tel" class="form-control" placeholder="Phone Number" id="phone" required
+                                   data-validation-required-message="Please enter your phone number.">
+                            <p class="help-block text-danger"></p>
+                        </div>
+                    </div>
+                    <div class="row control-group">
+                        <div class="form-group col-xs-12 floating-label-form-group controls">
+                            <label for="message">Message</label>
+                            <textarea name="message" rows="5" class="form-control" placeholder="Message" id="message" required
+                                      data-validation-required-message="Please enter a message."></textarea>
+                            <p class="help-block text-danger"></p>
+                        </div>
+                    </div>
+                    <br>
+                    <div id="success"></div>
+                    <div class="row">
+                        <div class="form-group col-xs-12">
+                            <button type="submit" class="btn btn-success btn-lg">Send</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Footer -->
+<footer class="text-center">
+    <div class="footer-above">
+        <div class="container">
+            <div class="row">
+                <div class="footer-col col-md-12">
+                    <h3>Connect</h3>
+                    <ul class="list-inline">
+                        <li>
+                            <a href="https://www.linkedin.com/in/christen-ward-771a5a110" target="_blank"
+                               class="btn-social btn-outline"><span class="sr-only">LinkedIn</span><i
+                                        class="fa fa-fw fa-linkedin"></i></a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/QuinceP" target="_blank" class="btn-social btn-outline"><span
+                                        class="sr-only">Github</span><i
+                                        class="fa fa-fw fa-github"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-below">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    Copyright &copy; Christen Ward 2017
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<div class="scroll-top page-scroll hidden-sm hidden-xs hidden-lg hidden-md">
+    <a class="btn btn-primary" href="#page-top">
+        <i class="fa fa-chevron-up"></i>
+    </a>
+</div>
+
+<!-- Portfolio Modals -->
+<div class="portfolio-modal modal fade" id="portfolioModal1" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-content">
+        <div class="close-modal" data-dismiss="modal">
+            <div class="lr">
+                <div class="rl">
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2">
+                    <div class="modal-body">
+                        <h2>Echo - A Memory Game</h2>
+                        <hr class="star-primary">
+                        <img src="img/portfolio/echo.png" class="img-responsive img-centered" alt="">
+                        <p>Echo is a puzzle game of memory and concentration for Android.
+                            Wait for the lights to flash in the sequence, and see if you can remember the order.
+                            How high of a score can you achieve?
+                            <a href='https://play.google.com/store/apps/details?id=com.echo.game.android&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/images/generic/en_badge_web_generic.png'/></a>
+                        </p>
+                        <ul class="list-inline item-details">
+                            <li>Languages/Frameworks:
+                                <strong>Java/LibGDX
+                                </strong>
+                            </li>
+                        </ul>
+                        <button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-times"></i>
+                            Close
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="portfolio-modal modal fade" id="portfolioModal2" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-content">
+        <div class="close-modal" data-dismiss="modal">
+            <div class="lr">
+                <div class="rl">
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2">
+                    <div class="modal-body">
+                        <h2>Dungeon Crawl</h2>
+                        <hr class="star-primary">
+                        <img src="img/portfolio/dc.png" class="img-responsive img-centered" alt="">
+                        <p>A character generator complete with statistics, for usage with a tabletop roleplaying game. Uses dice rolls and
+                            calculations to randomize a character for a given class. The battle simulator simulates a battle given a set of rules
+                            and damage formulas.
+                        </p>
+                        <ul class="list-inline item-details">
+                            <li>Languages/Frameworks:
+                                <strong>C++, SMFL
+                                </strong>
+                            </li>
+                        </ul>
+                        <button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-times"></i>
+                            Close
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="portfolio-modal modal fade" id="portfolioModal3" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-content">
+        <div class="close-modal" data-dismiss="modal">
+            <div class="lr">
+                <div class="rl">
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2">
+                    <div class="modal-body">
+                        <h2>Space Roguelike </h2>
+                        <hr class="star-primary">
+                        <img src="img/portfolio/space.png" class="img-responsive img-centered" alt="">
+                        <p>A work in progress. This is a sci-fi roguelike developed with Pygame, featuring turn based combat, quests, and plenty of procedural generation.</p>
+                        <ul class="list-inline item-details">
+                            <li>Languages/Frameworks:
+                                <strong> Python, Pygame
+                                </strong>
+                            </li>
+                        </ul>
+                        <button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-times"></i>
+                            Close
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- jQuery -->
+<script src="js/jquery.min.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+
+<!-- Plugin JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+
+<!-- Contact Form JavaScript -->
+<script src="js/jqBootstrapValidation.js"></script>
+
+<!-- Theme JavaScript -->
+<script src="js/freelancer.min.js"></script>
+
+<script src="js/contact_me.js"></script>
+
+</body>
+
+</html>
